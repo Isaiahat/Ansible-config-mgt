3b5352ebc3a204925b07fc7a8b1bec6a47e1cf69
Isaiahat <toluwaniadeoti@gmail.com>
2024-06-26 23:34:14 +0100

    new jenkins setup on ubuntu

--- websetup.sh
+++ websetup.sh
@@ -36,4 +36,4 @@ echo "Removing Temporary Files"
 echo "########################################"
 sudo rm -rf /tmp/webfiles
 echo "job done"
-echo "all men are mortals" 
+echo "all men are mortalss" 
