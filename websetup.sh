7de48fef4fa5f210ed70eb08a49300334a139a98
Isaiahat <toluwaniadeoti@gmail.com>
2024-06-30 06:24:55 +0100

    still testing

--- websetup.sh
+++ websetup.sh
@@ -4,37 +4,37 @@
 echo "########################################"
 echo "Installing packages."
 echo "########################################"
-sudo apt update -y > /dev/null
-sudo apt install wget unzip apache2 -y > /dev/null
+#sudo apt update -y > /dev/null
+#sudo apt install wget unzip apache2 -y > /dev/null
 echo
 
 # Creating Temp Directory
 echo "########################################"
 echo "Starting Artifact Deployment"
 echo "########################################"
-mkdir -p /tmp/webfiles
+#mkdir -p /tmp/webfiles
 cd /tmp/webfiles
 echo
 
-wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
-unzip 2137_barista_cafe.zip > /dev/null
-sudo cp -r 2137_barista_cafe/* /var/www/html/
+#wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
+#unzip 2137_barista_cafe.zip > /dev/null
+#sudo cp -r 2137_barista_cafe/* /var/www/html/
 echo
 
 # Bounce Service
 echo "########################################"
 echo "Restarting Apache Webserver service"
 echo "########################################"
-sudo systemctl start apache2
-sudo systemctl enable apache2
-sudo systemctl restart apache2
-echo
+#sudo systemctl start apache2
+#sudo systemctl enable apache2
+#sudo systemctl restart apache2
+
 
 # Clean Up
 echo "########################################"
 echo "Removing Temporary Files"
 echo "########################################"
-sudo rm -rf /tmp/webfiles
+#sudo rm -rf /tmp/webfiles
 echo "job done"
-echo "all men are mortals and socrates is a man" 
 mkdir /home/ubuntu/test_folder
+echo "test folder created"
